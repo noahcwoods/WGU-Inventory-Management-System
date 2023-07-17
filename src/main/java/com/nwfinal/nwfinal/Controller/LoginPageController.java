6978c38b396d211de09d4fcdb6b738bdd28c282f
Noah <noahcwoods@gmail.com>
2023-07-17 19:23:37 -0400

    Day 2 electric boogaloo

---
+++ src/main/java/com/nwfinal/nwfinal/Controller/LoginPageController.java
@@ -0,0 +1,23 @@
+package com.nwfinal.nwfinal.Controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class LoginPageController {
+
+    @FXML
+    private TextField userLoginTxt;
+
+    @FXML
+    private TextField userPasswordTxt;
+
+    @FXML
+    private Label zoneIDLbl;
+
+    @FXML
+    void onActionClickLoginBtn(ActionEvent event) {
+    }
+
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,11 @@
 module com.nwfinal.nwfinal {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
 
     opens com.nwfinal.nwfinal to javafx.fxml;
+    opens com.nwfinal.nwfinal.Controller to javafx.fxml;
     exports com.nwfinal.nwfinal;
+    exports com.nwfinal.nwfinal.Controller;
 }
